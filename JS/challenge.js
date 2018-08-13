465c5eeebda4b02776432053ddb8e5a0ed75b9dd
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-13 10:37:44 -0600

    added css file for highlighting purposes. Added highlight function in JS. Changed paragraph in html and added divs

--- JS/challenge.js
+++ JS/challenge.js
@@ -1,1 +1,9 @@
-
+function highlight(text) {
+	let inputText = document.getElementById("p1");
+	let innerHTML = inputText.innerHTML;
+	let index = innerHTML.indexOf(text);
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+		inputText.innerHTML = innerHTML;
+	}
+}
